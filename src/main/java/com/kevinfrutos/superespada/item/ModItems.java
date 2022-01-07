4b4f2497aa56991f462547c4b9c225d881fcec91
KevinFrutos <vinke.14297@gmail.com>
2022-01-07 18:30:38 +0100

    v1.16.5-1.0.3 Armor added. Provisional assets

--- src/main/java/com/kevinfrutos/superespada/item/ModItems.java
+++ src/main/java/com/kevinfrutos/superespada/item/ModItems.java
@@ -2,6 +2,8 @@ package com.kevinfrutos.superespada.item;
 
 import com.kevinfrutos.superespada.SuperEspada;
 import com.kevinfrutos.superespada.item.custom.ExcaliburItem;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
@@ -12,12 +14,32 @@ public class ModItems {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, SuperEspada.MOD_ID);
 
+    // ITEMS
+    public static final RegistryObject<Item> GOD_GEM = ITEMS.register("god_gem",
+            () -> new Item(new Item.Properties().group(ModItemGroup.SUPERESPADA_GROUP)));
+
+    // WEAPONS
     public static final RegistryObject<Item> EXCALIBUR = ITEMS.register("excalibur",
             () -> new ExcaliburItem(ModItemTier.GOD_GEM, 5, 5f,
                     new Item.Properties().maxStackSize(1).group(ModItemGroup.SUPERESPADA_GROUP)));
 
-    public static final RegistryObject<Item> GOD_GEM = ITEMS.register("god_gem",
-            () -> new Item(new Item.Properties().group(ModItemGroup.SUPERESPADA_GROUP)));
+    //ARMOR
+    public static final RegistryObject<Item> GOD_GEM_HELMET = ITEMS.register("god_gem_helmet",
+            () -> new ArmorItem(ModArmorMaterial.GOD_GEM, EquipmentSlotType.HEAD,
+                    new Item.Properties().group(ModItemGroup.SUPERESPADA_GROUP)));
+
+    public static final RegistryObject<Item> GOD_GEM_CHESTPLATE = ITEMS.register("god_gem_chestplate",
+            () -> new ArmorItem(ModArmorMaterial.GOD_GEM, EquipmentSlotType.CHEST,
+                    new Item.Properties().group(ModItemGroup.SUPERESPADA_GROUP)));
+
+    public static final RegistryObject<Item> GOD_GEM_LEGGINGS = ITEMS.register("god_gem_leggings",
+            () -> new ArmorItem(ModArmorMaterial.GOD_GEM, EquipmentSlotType.LEGS,
+                    new Item.Properties().group(ModItemGroup.SUPERESPADA_GROUP)));
+
+    public static final RegistryObject<Item> GOD_GEM_BOOTS = ITEMS.register("god_gem_boots",
+            () -> new ArmorItem(ModArmorMaterial.GOD_GEM, EquipmentSlotType.FEET,
+                    new Item.Properties().group(ModItemGroup.SUPERESPADA_GROUP)));
+
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
